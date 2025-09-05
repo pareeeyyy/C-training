3d5bab88be506ab3f0ac65d4e830a7d8a0f74609
pareeeyyy <paripiku1506@gmail.com>
2025-09-05 13:41:18 +0530

    Initial commit

---
+++ loop.c
@@ -0,0 +1,53 @@
+// using for loop print numbers 1 to 10
+// #include <stdio.h>
+// int main(){
+//     //printf("%d\n",1);
+//     //printf("%d\n",2);
+//     //printf("%d\n",3);
+//     //printf("%d\n",4);
+//     //printf("%d\n",5);
+//     //printf("%d\n",6);
+//     //printf("%d\n",7);
+//     //printf("%d\n",8);
+//     //printf("%d\n",9);
+//     //printf("%d\n",10):
+//     for(int i=10;i>0;i--){
+//         printf("%d\n",i);
+//     }
+//     return 0;
+
+
+// }
+
+
+
+
+
+#include <stdio.h>
+int main(){
+    for (int i=1 ;i<=100;i++){
+        printf("%d\n",i);
+    
+    }
+    return 0;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
